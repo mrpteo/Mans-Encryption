3f1b758f1697d9612f9afa399ccfe38ec0550eca
mrpteo <peterarnell12@gmail.com>
2014-11-07 18:56:26 +0000

    Latest Version, WIP
    Still a work in progress as there is a flaw in the key generation. Fails
    every so often and messages cannot be decrypted. If key generates fine
    then all is well and it will always work.

--- multi-aplha_encoding.rb
+++ multi-aplha_encoding.rb
@@ -1,34 +1,88 @@
 class Encrypt
 		#arrays with lettes stored		
-		$alphabet    = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '] 
-
-		$array_one   = ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a']
-		$array_two   = ['w','b','y','r','v','e','q','z','a','y','t','u','f','d','x','m','k','c','n','j','i','g','h','l','o','p']
-		$array_three = ['l','u','q','o','r','g','a','j','z','c','h','v','i','y','f','w','k','e','t','p','s','d','x','b','n','m']
-		$array_four  = ['f','w','k','e','t','o','r','g','a','j','p','s','d','x','b','n','m','l','u','q','z','c','h','v','i','y']
-		$array_five  = ['h','v','i','y','p','s','d','x','b','n','f','w','k','l','u','q','o','z','c','e','t','m','r','g','a','j']
-
+	$alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g',
+				'h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','<','>','.',',','&','£','+','[',']','{','}',"'",'@','!',
+				'"','$','%','^','*','(',')','-','_','=','+','1','2','3','4','5','6','7','8','9','0',' '] 
 		$array = []
 
 	def generate_key(key_strength, output_file)
 		#generate key is designed so each user can create a unique set of mixed alphabets to increate security.
 
-		random_number = Random.new
+		for count in 0..key_strength - 1
+			#loop through the code to produe ...(key_strength) amount of arrays
+			#check if the array is defined
 
-		#next job!
+			store = []
 
+			if defined? $array[count] == false
+				$array.push(Array.new())
+			end
+
+			rand = Random.new
+			#loop through every char in $alphabet
+			for alpha_char_count in 0..$alphabet.length - 1
+
+				random_number = rand(0..$alphabet.length - 1)
+					#generate random letter
+				random_letter = $alphabet[random_number.to_i]
+
+					#check if iv already added that random letter to the array else add it
+				if $array[count].include?(random_letter)
+
+					#redo the loop to try again
+					redo
+				else
+
+					#push random char to the array
+					$array[count].push(random_letter)
+					store.push(random_letter)
+					$alphabet.delete(random_letter)
+				end
+			end
+
+			for letter in store
+				$alphabet.push(letter)
+			end
+		end
+
+		file = File.open(output_file, "w") do |file|
+
+			#loop through each sub-array
+			for i in 0..$array.length - 1
+				#loop through $array and output them into an array e.g. z,e,t,f...
+
+				file_line = ""
+
+				#loop through chars and build file line
+				for count in 0..$alphabet.length - 1
+					file_line = file_line + $array[i][count].to_s
+				end
+
+				#print file line and reset variable
+				file.puts(file_line)
+				file_line = ""
+			end
+		end
 	end
 
 	def load_key(input_file)
 		#load previous keyfile, allows for decoding messages
+		file = File.open(input_file)
 
+		count = 0
+
+		file.each_line{ |file_line|
+
+			line_split = file_line.split("")
+
+			line_split.delete("\n")
+
+			$array.push(line_split)
+		}
 	end
 
 
 	def encrypt(string)
-		#just to simplify it and make everything lowercase
-		string = string.downcase
-
 		#encrypted string
 		new_string = ""
 
@@ -41,114 +95,70 @@ class Encrypt
 		#get array index
 		hash = Hash[$alphabet.map.with_index.to_a]
 
-		#for later to decide which array to use
-		array_number = 1
+		count = 0
 
-		#to reference each letter in the string
+		#loop through each charcater, loop through the number of arrays...
 		for letter in split_raw 
 
-			#make sure the character is in the array, e.g. no special chars
 			if $alphabet.include? letter
 
 				#make sure the hash index is an integer
 				number = hash[letter].to_i
-
 				#dont add anything for spaces, easily changed to accomodate spaces
-				if letter != " "
-					#decide which letter to swap and with what array
-					case array_number
-						when 1
-							new_string = new_string + $array_one[number]
-							array_number += 1
-						next
-						when 2
-							new_string = new_string + $array_two[number]
-							array_number += 1
-						next
-						when 3
-							new_string = new_string + $array_three[number]
-							array_number += 1
-						next
-						when 4
-							new_string = new_string + $array_four[number]
-							array_number += 1
-						next
-						when 5
-							new_string = new_string + $array_five[number]
-							array_number = 1
-						next
-					end
-				else
-					new_string = new_string + "1"
+				
+				new_string = new_string + $array[count][number].to_s
+					
+				count += 1
+				if count > $array.length - 1
+					count = 0
 				end
 			end
+
 		end
 
 		#return encoded string
 		return new_string
 	end
 
-	def decode(encoded_string)
+	def decrypt(encrypted_string)
 		#allow me to reference each element within each array by its index
 		#a bit messy but it does the job
-		hash1 = Hash[$array_one.map.with_index.to_a]
-		hash2 = Hash[$array_two.map.with_index.to_a]
-		hash3 = Hash[$array_three.map.with_index.to_a]
-		hash4 = Hash[$array_four.map.with_index.to_a]
-		hash5 = Hash[$array_five.map.with_index.to_a]
 
 		#split the encoded string
-		string_split = encoded_string.split("")
+		string_split = encrypted_string.split("")
 
-		#array selector
-		array_number = 1
+		array_count = 0
 
 		#new string variable
 		new_string = ""
 
-		#for loop to reference each character
 		for letter in string_split
 
-			#check for spaces
-			if letter != "1"
+			hash = Hash[$array[array_count].map.with_index.to_a]
 
-				#get index number of each character
-				number1 = hash1[letter].to_i	
-				number2 = hash2[letter].to_i
-				number3 = hash3[letter].to_i
-				number4 = hash4[letter].to_i
-				number5 = hash5[letter].to_i
+			number = hash[letter].to_i
 
-				#decide which array to use
-				case array_number
-					when 1
-						new_string = new_string + $alphabet[number1]
-						array_number += 1
-					next
-					when 2
-						new_string = new_string + $alphabet[number2]
-						array_number += 1
-					next
-					when 3
-						new_string = new_string + $alphabet[number3]
-						array_number += 1
-					next
-					when 4
-						new_string = new_string + $alphabet[number4]
-						array_number += 1
-					next
-					when 5
-						new_string = new_string + $alphabet[number5]
-						array_number = 1
-					next
-				end
+			new_string = new_string + $alphabet[number].to_s
+
+			if array_count >= $array.length - 1
+				array_count = 0
 			else
-				#reenter spaces into ou text
-				new_string = new_string + " "
+				array_count += 1	
 			end
 		end
-
 		#return the decoded string
 		return new_string
 	end
 end
+
+start = Encrypt.new
+
+#encoded_string = start.encrypt("Welcome to my world wont you come on in This is lots of fun")
+#puts(encoded_string)
+#puts(start.decode(encoded_string))
+
+start.load_key("key.txt")
+
+encrypted = start.encrypt("Hey there world, this is my own encryption i've been working on. You should all let me know what you think. The key is randomly generated and will sometimes fail, hence the script needs to be run a number of times to be 100%. No need for public encryptions...")
+puts("\n\n" + encrypted + "\n\n----------------------------------------------------\n\n")
+puts(start.decrypt(encrypted))
